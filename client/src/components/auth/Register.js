f158662d6f4e7f725d70d7460d5e5d84f969c615
oruxas <evaldaskal@gmail.com>
2021-04-25 12:45:10 +0300

    User Register api connected & alert set

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,21 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import AlertContext from './../../context/alert/alertContext';
-
+import AuthContext from './../../context/auth/authContext';
 
  const Register = () => {
     const alertContext = useContext(AlertContext)
+    const authContext = useContext(AuthContext)
 
     const {setAlert} = alertContext
+    const {register, error, clearErrors} = authContext
+
+    useEffect(() => {
+        // Not a good practice but for small test app will work
+        if (error === 'User already exists') {
+            setAlert(error, 'danger')
+            clearErrors()
+        }
+    }, [error])
 
      const [user, setUser] = useState({
          name: '',
@@ -25,7 +35,11 @@ import AlertContext from './../../context/alert/alertContext';
          } else if (password !== password2) {
              setAlert('Passwords doesn\'t match', 'warning')
          } else {
-            console.log('Register Submit')
+             register({
+                 name,
+                 email,
+                 password
+             })
          }
      }
     return (
